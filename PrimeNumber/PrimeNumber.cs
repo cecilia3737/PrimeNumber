af74fb4ec3dfcb04c050f6cc705d7fd490784a3a
Cecilia Nilsson <cecilia.o.nilsson@live.se>
2021-10-11 14:24:25 +0200

    Added algoritm that check if input is a prime number and added SearchPrime-method. It checks if user input is valid.

--- PrimeNumber/PrimeNumber.cs
+++ PrimeNumber/PrimeNumber.cs
@@ -14,6 +14,7 @@ namespace PrimeNumber
 
             bool run = true;
 
+            //Runs startmenu until user quits
             while (run)
             {
                 PrintMenu();
@@ -24,10 +25,10 @@ namespace PrimeNumber
                 { 
                 
                     case "1":
-                        //SearchPrime();
+                        SearchPrime(primeList);
                         break;
                     case "2":
-                        //ListPrime();
+                        //ListPrime(primeList);
                         break;
                     case "3":
                         run = false;
@@ -39,21 +40,65 @@ namespace PrimeNumber
 
             }
 
-
         }
 
         static void PrintMenu()
         {
             Console.WriteLine("" +
-                    "1. Search for prime\n" +
-                    "2. Show list of prime\n" +
+                    "1. Search for a prime number\n" +
+                    "2. Show list of prime numbers\n" +
                     "3. Quit\n" +
                     "----------");
             Console.WriteLine("Enter your choice:");
         }
 
-        static void SearchPrime()
+        /*Accept an interger input from user, If input is invalid, it will print an error and asks for a new input
+          If input is not a prime number, it will print an error message saying so.
+          If input is a prime number it will print a message and proceed to add the number to a list of Prime numbers)*/
+        static void SearchPrime(List<int> list)
         {
+            bool runPrimeSearch = true;
+
+            Console.WriteLine("---------- \n" +
+                              "Enter a number:");
+
+            while (runPrimeSearch)
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine("----------");
+
+                int num;
+
+                if(int.TryParse(input, out num))
+                {
+                    if (CheckIfPrime(num))
+                    {
+                        Console.WriteLine($"{num} is a prime number");
+
+                        if (!list.Contains(num))
+                        {
+                            list.Add(num);
+                        }
+
+                    }
+
+                    else
+                    {
+                        Console.WriteLine($"{num} is not a prime number");
+                    }
+
+                    runPrimeSearch = false;
+
+                }
+
+                else
+                {
+                    Console.WriteLine($"Wrong input, Please enter a number");
+                }
+            
+            }
+
+            Console.WriteLine("----------");
 
         }
 
@@ -68,6 +113,32 @@ namespace PrimeNumber
             Console.WriteLine("---------- \n" +
                     "Wrong input, Enter a number between 1-3 \n" +
                     "----------");
+        }
+
+        /*Algoritm that checks if number is prime or not.
+          First it checks if a input is 1 or less - Can't be a primenumber if so.
+          i is the numbers between 2 and input divided by 2.
+          If there are remainder when input is divided by any of numbers i, then it will show up true (It is a prime number), 
+          if zero remainder it will show up false.*/
+        private static bool CheckIfPrime(int input)
+        {
+
+            var isPrime = true;
+
+            if (input <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= input / 2; i++)
+            {
+                if (input % i == 0)
+                {
+                    isPrime = false;
+                }
+            }
+
+            return isPrime;
         }
 
     }
